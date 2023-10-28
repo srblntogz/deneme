bdeebdcb633be3436c6474448408078d5a262857
srblntogz <47463091+srblntogz@users.noreply.github.com>
2023-10-28 15:11:33 +0300

    Update page.js

--- app/Link1/page.js
+++ app/Link1/page.js
@@ -1,9 +1,28 @@
+"use client";
+
+import React, { useState, useEffect } from 'react';
+import { DataTable } from 'primereact/datatable';
+import { Column } from 'primereact/column';
 
 
 export default function Link1(){
+  const [items, setitems] = useState([]);
+
+
+  useEffect(() => {
+    fetch('https://api.epias.com.tr/epias/exchange/transparency/market/day-ahead-interim-mcp?date=2023-10-10').then(function (responce) {
+        return responce.json();
+    }).then(function (result) {
+        setitems(result);
+    });
+}, []);
+
     return(
-        <div>
-          <input type="date"/>
+      <div className="container p-3">
+            <DataTable value={items} paginator rows={10} rowsPerPageOptions={[5, 10, 25, 50]} tableStyle={{ minWidth: '50rem' }}>
+                <Column field="date" header="date"></Column>
+          
+            </DataTable>
         </div>
     )
 }
